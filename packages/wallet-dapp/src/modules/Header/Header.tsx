3e370139f34fe6163b7f14730ea9eb55f51d7a7b
lovro14 <lovro.vidovic12@gmail.com>
2023-07-27 23:12:15 +0200

    fix: added modal to header

--- packages/wallet-dapp/src/modules/Header/Header.tsx
+++ packages/wallet-dapp/src/modules/Header/Header.tsx
@@ -1,21 +1,40 @@
 import Jazzicon from 'react-jazzicon'
+import { useCallback, useState } from 'react'
 
 import Typography from 'components/Typography/Typography'
 import Select, { Option } from 'components/Select/Select'
 import { Wrapper } from './styles'
 import { ellipsizeTokenAddress } from 'utils/tokenAddress'
+import Modal from 'components/Modal/Modal'
+import ModalTitle from 'components/Modal/components/ModalTitle'
+import ModalBody from 'components/Modal/components/ModalBody'
+
+const address = '0xcc2bd176a478baea9a0de7a24cd927661cc6e860d5bacecb9a138ef20dbab231'
 
 const Header = () => {
+  const [isOpenInfoModal, setIsOpenInfoModal] = useState(false)
+
+  const toggleModal = useCallback(() => {
+    setIsOpenInfoModal(!isOpenInfoModal)
+  }, [isOpenInfoModal])
+
   const handleOptionClick = (o: Option) => {
     console.log('handleOptionClick')
   }
+
+  const handleAddressClick = useCallback(() => navigator.clipboard.writeText(address), [])
+
   return (
     <Wrapper>
       <div style={{ display: 'flex', alignItems: 'center' }}>
-        <Jazzicon diameter={25} seed={Math.round(Math.random() * 10000000)} />
-        <Typography variant="body" style={{ marginLeft: 12 }}>
-          {ellipsizeTokenAddress('0xcc2bd176a478baea9a0de7a24cd927661cc6e860d5bacecb9a138ef20dbab231')}
-        </Typography>
+        <div onClick={toggleModal} style={{ cursor: 'pointer' }}>
+          <Jazzicon diameter={25} seed={Math.round(Math.random() * 10000000)} />
+        </div>
+        <div style={{ cursor: 'pointer' }} onClick={handleAddressClick}>
+          <Typography variant="body" style={{ marginLeft: 12 }}>
+            {ellipsizeTokenAddress(address)}
+          </Typography>
+        </div>
       </div>
       <Select
         options={[
@@ -24,6 +43,12 @@ const Header = () => {
         ]}
         onOptionClick={handleOptionClick}
       />
+      {isOpenInfoModal && (
+        <Modal onClose={toggleModal}>
+          <ModalTitle onClose={toggleModal}>Info</ModalTitle>
+          <ModalBody>Info details</ModalBody>
+        </Modal>
+      )}
     </Wrapper>
   )
 }
