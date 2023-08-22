09f747ba746a33f4464f5335d4070f4e085a1af6
lovro14 <lovro.vidovic12@gmail.com>
2023-08-22 20:21:17 +0200

    updated number formatting

--- packages/wallet-dapp/src/utils/formatting.ts
+++ packages/wallet-dapp/src/utils/formatting.ts
@@ -1,4 +1,7 @@
 export const formatNumberWithCommas = (numberStr: string) => {
+  if (!numberStr) {
+    return ''
+  }
   const number = Number(numberStr)
   if (Number.isFinite(number) === false) return numberStr
   return new Intl.NumberFormat('en-US', {
