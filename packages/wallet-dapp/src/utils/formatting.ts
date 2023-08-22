86f4965f0abbc95c4bf8361f74fcedb22e0628e3
Krešimir Klas <github@kklas.net>
2023-08-22 08:07:16 +0000

    fix formatNumberWithCommans

--- packages/wallet-dapp/src/utils/formatting.ts
+++ packages/wallet-dapp/src/utils/formatting.ts
@@ -1,3 +1,7 @@
 export const formatNumberWithCommas = (numberStr: string) => {
-  return numberStr.replace(/\B(?=(\d{3})+(?!\d))/g, ',')
+  const number = Number(numberStr)
+  if (Number.isFinite(number) === false) return numberStr
+  return new Intl.NumberFormat('en-US', {
+    maximumFractionDigits: 15,
+  }).format(number)
 }
