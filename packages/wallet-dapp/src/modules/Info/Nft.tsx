b456600377374aecbec06aee1e0d38e19ff6d985
lovro14 <lovro.vidovic12@gmail.com>
2023-09-21 22:13:15 +0200

    Added nft tab

--- packages/wallet-dapp/src/modules/Info/Nft.tsx
+++ packages/wallet-dapp/src/modules/Info/Nft.tsx
@@ -1,3 +1,169 @@
+import { useCallback, useEffect, useState } from 'react'
+import styled from 'styled-components'
+
+import { IconNftPlaceholder } from 'components/Icons/IconNftPlaceholder'
+import Spinner from 'components/Spinner'
+import { WALLET_BALANCES_REFETCH_INTERVAL } from 'utils/const'
+import { useOwnedObjects } from 'utils/useOwnedObjects'
+import Typography from 'components/Typography'
+import { ellipsizeTokenAddress } from 'utils/helpers'
+
+const Container = styled.div`
+  padding-top: 16px;
+  padding-left: 24px;
+  padding-right: 24px;
+  display: flex;
+  flex-wrap: wrap;
+  gap: 8px;
+`
+
+const EmptyPlaceholder = styled.div`
+  width: 144px;
+  height: 144px;
+  border-radius: 13px;
+  background: #f2f4f6;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  cursor: pointer;
+  position: relative;
+  .hide {
+    display: none;
+  }
+  &:hover {
+    & > {
+      .hide {
+        display: block;
+        position: absolute;
+        top: 30px;
+      }
+    }
+  }
+`
+
+const PlaceholderWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 100%;
+  .hide {
+    display: none;
+  }
+`
+
+const StyledNftImage = styled.img`
+  width: 144px;
+  height: 144px;
+  border-radius: 13px;
+  cursor: pointer;
+
+  &:hover + .hide {
+    display: block;
+    position: absolute;
+  }
+`
+
+const ImgWrapper = styled.div`
+  display: flex;
+  position: relative;
+  .hide {
+    display: none;
+  }
+`
+
+const NftName = styled(Typography)`
+  background-color: rgba(255, 255, 255, 0.95);
+  border: 1px solid #d6d9dc;
+  padding: 4px 0;
+  border-radius: 4px;
+  width: 110px;
+  left: 50%;
+  transform: translateX(-50%);
+  bottom: 16px;
+  text-align: center;
+`
+const NftType = styled(Typography)`
+  width: 110px;
+  left: 50%;
+  transform: translateX(-50%);
+  bottom: 16px;
+  text-align: center;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  top: inherit !important;
+`
+
+const NftImage = ({ imgSrc, objectId, name }: { imgSrc?: string; objectId?: string; name?: string }) => {
+  const [showPlaceholder, setShowPlaceholder] = useState(false)
+
+  useEffect(() => setShowPlaceholder(!imgSrc), [imgSrc])
+
+  const handleImageLoaded = useCallback(() => setShowPlaceholder(false), [])
+
+  const handleImageLoadError = useCallback(() => setShowPlaceholder(true), [])
+
+  return showPlaceholder ? (
+    <EmptyPlaceholder key={objectId}>
+      <PlaceholderWrapper>
+        <IconNftPlaceholder />
+      </PlaceholderWrapper>
+      {name ? (
+        <NftName variant="caption" className="hide">
+          {name}
+        </NftName>
+      ) : null}
+    </EmptyPlaceholder>
+  ) : (
+    <ImgWrapper>
+      <StyledNftImage key={objectId} onLoad={handleImageLoaded} onError={handleImageLoadError} src={imgSrc} />
+      {name ? (
+        <NftName variant="caption" className="hide">
+          {name}
+        </NftName>
+      ) : null}
+    </ImgWrapper>
+  )
+}
+
 export const Nft = () => {
-  return <div>Nft</div>
+  const { isLoading, ownedObjects } = useOwnedObjects({ refetchInterval: WALLET_BALANCES_REFETCH_INTERVAL })
+
+  if (isLoading) {
+    return <Spinner style={{ marginTop: 48 }} />
+  }
+
+  if (!ownedObjects) {
+    return null
+  }
+
+  return (
+    <Container>
+      {ownedObjects.map(o => {
+        if (o.data?.display?.data === null) {
+          const type = o.data?.type || ''
+          const address = type ? type.substring(0, type.indexOf('::')) : ''
+
+          console.log(type.substring(type.indexOf('::')))
+          return (
+            <EmptyPlaceholder key={o.data?.objectId}>
+              <PlaceholderWrapper>
+                <IconNftPlaceholder />
+              </PlaceholderWrapper>
+              <NftType variant="caption" className="hide">
+                <div>{ellipsizeTokenAddress(address)}</div>
+                <div style={{ overflow: 'hidden', textOverflow: 'ellipsis', whiteSpace: 'nowrap' }}>
+                  {type.substring(type.indexOf('::'))}
+                </div>
+              </NftType>
+            </EmptyPlaceholder>
+          )
+        }
+        const imgUrl = o.data?.display?.data?.['image_url' as keyof typeof o.data.display.data] as string
+        const name = o.data?.display?.data?.['name' as keyof typeof o.data.display.data] as string
+
+        return <NftImage key={o.data?.objectId} objectId={o.data?.objectId} imgSrc={imgUrl} name={name} />
+      })}
+    </Container>
+  )
 }
---
+++ packages/wallet-dapp/src/utils/useOwnedObjects.ts
@@ -0,0 +1,42 @@
+import { useQuery } from '@tanstack/react-query'
+import { useWalletKit } from '@mysten/wallet-kit'
+import { SuiObjectResponse } from '@mysten/sui.js'
+
+import { useSuiClientProvider } from './useSuiClientProvider'
+import { useNetwork } from './useNetworkProvider'
+
+interface OwnedObjectsInfos {
+  isLoading: boolean
+  ownedObjects?: SuiObjectResponse[]
+}
+
+export const useOwnedObjects = (options?: { refetchInterval: number }): OwnedObjectsInfos => {
+  const suiClient = useSuiClientProvider()
+  const { currentAccount } = useWalletKit()
+  const { network } = useNetwork()
+
+  const ownedObjectsRes = useQuery({
+    queryKey: ['owned-objects', currentAccount?.address, network],
+    enabled: !!currentAccount?.address,
+    queryFn: async () => {
+      if (!currentAccount?.address) {
+        throw new Error('invariant violation')
+      }
+
+      const ownedObjects = await suiClient.getOwnedObjects({
+        owner: currentAccount.address,
+        options: { showType: true, showDisplay: true },
+      })
+
+      return ownedObjects
+    },
+    refetchInterval: options?.refetchInterval,
+  })
+
+  const isLoading = ownedObjectsRes.isLoading
+
+  return {
+    isLoading,
+    ownedObjects: ownedObjectsRes.data?.data,
+  }
+}
